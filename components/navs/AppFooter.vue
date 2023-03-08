5c34b8811b003f68679ccbc2f00879143c79a5a1
Gareth Fuller <gareth.a.f@gmail.com>
2023-03-08 20:14:49 +0000

    chore: Add footer

---
+++ components/navs/AppFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="text-center border p-4 bg-gray-300">
+    <div class="flex justify-center items-center gap-8">
+      <component
+        v-for="(link, i) in links"
+        :key="`footer-link-${i}`"
+        :is="link.tag"
+        v-bind="link.attrs"
+        class="text-sm text-gray-600 hover:text-black transition-all duration-300">
+        {{  link.label }}
+      </component>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+
+  data () {
+    return {
+      links: [
+        { label: 'about', tag: 'nuxt-link', attrs: { to: '/about' } },
+        { label: 'blog', tag: 'nuxt-link', attrs: { to: '/blog' } },
+        { label: 'bip', tag: 'nuxt-link', attrs: { to: '/bip' } },
+        { label: 'github', tag: 'a', attrs: { href: 'https://github.com/Invoice-build', target: '_blank', rel: 'noreferrer' } },
+        { label: 'discord', tag: 'a', attrs: { href: 'https://discord.gg/ymKtqwPjcP', target: '_blank', rel: 'noreferrer' } },
+        { label: 'twitter', tag: 'a', attrs: { href: 'https://twitter.com/invoicebuild', target: '_blank', rel: 'noreferrer' } }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Nuxt />
+    <app-footer />
     <app-nav />
     <app-snackbars />
   </div>
@@ -10,11 +11,13 @@
 import { mapActions } from 'vuex'
 import AppNav from '~/components/navs/AppNav.vue'
 import AppSnackbars from '~/components/alerts/AppSnackbars.vue'
+import AppFooter from '~/components/navs/AppFooter.vue'
 
 export default {
   components: {
     AppNav,
-    AppSnackbars
+    AppSnackbars,
+    AppFooter
   },
 
   async beforeMount () {
